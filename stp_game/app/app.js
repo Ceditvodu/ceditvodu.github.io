71ac1dbe004198ad0f72315a2236df82edc1ceea
Ceditvodu <ceditvodu@gmail.com>
2016-12-22 19:00:06 +0200

    dependensy minimising

--- stp_game/app/app.js
+++ stp_game/app/app.js
@@ -10,65 +10,104 @@ window.onload = () => {
 function programStart (container) {
 	let program_cont = document.getElementById(container);
 
-	var land = [
-		[
-			{
-				"id": 1,
-				"type":"grass",
-			},
-			{
-				"id": 2,
-				"type":"grass1"
-			}
-		],	
-		[
-			{
-				"id": 3,
-				"type":"grass"
-			},
-			{
-				"id": 4,
-				"type":"grass1"
-			}
-		],
-		[
-			{
-				"id": 5,
-				"type":"grass2"
-			},
-			{
-				"id": 6,
-				"type":"grass3"
-			}
-		]
-	]
-
+	// collector it is entity that helps to keep records 
+	// of loading resources
 	let collector = new Worker("app/assets/collector.js");
 	collector.onmessage = (e) => {
 		console.dir(e.data);
 	}
 
-	let section_gen = new Worker("app/view/section_gen.js");
-	section_gen.postMessage('make');
-	section_gen.onmessage = (e) => {
-		program_cont.innerHTML = e.data;
-
-		collector.postMessage({
-			'module':'section_gen',
-			'action':'add'
-		});
+	let section_runner = {
+		sections:[
+			{
+				'callback': landConstrStart,
+				'args':{
+					'map':[
+						[
+							{
+								"id": 1,
+								"type":"grass",
+							},
+							{
+								"id": 2,
+								"type":"grass"
+							}
+						],	
+						[
+							{
+								"id": 3,
+								"type":"grass"
+							},
+							{
+								"id": 4,
+								"type":"grass"
+							}
+						],
+						[
+							{
+								"id": 5,
+								"type":"grass"
+							},
+							{
+								"id": 6,
+								"type":"grass"
+							}
+						]
+					]
+				}
+			},
+			{
+				'callback': sayHello,
+				'args':{
+					'map':['a']	
+				}
+			}
+		],
+		run: function(i = 0, callback){
+			this.sections[i]['callback'](this.sections[i].args, callback);
+			({
+				1:()=>{this.run(++i)},
+				0:()=>{}
+			}[+(i<this.sections.length-1)])();
+		}
 	}
 
-	let land_constr = new Worker("app/view/land_constr.js");
-	land_constr.postMessage(land);
-	land_constr.onmessage = (e) => {
-		let ground = document.getElementById('ground');
-		ground.innerHTML = e.data;
+	sectionGenStart(section_runner, program_cont, collector);
 
-		collector.postMessage({
-			'module':'lond_constr',
-			'action':'add'
-		});
+	function sectionGenStart (callbacks, container, collectors_entity) {
+		let section_gen = new Worker("app/view/section_gen.js");
+		section_gen.postMessage('make');
+		section_gen.onmessage = (e) => {
+			container.innerHTML = e.data;
+
+			collectors_entity.postMessage({
+				'module':'section_gen',
+				'action':'add'
+			});
+		}
+
+		callbacks.run(0, collectors_entity);
+	}
+
+	function landConstrStart(callback_args, collectors_entity){
+		let land_constr = new Worker("app/view/land_constr.js");
+		land_constr.postMessage(callback_args.map);
+		land_constr.onmessage = (e) => {
+			let ground = document.getElementById('ground');
+			ground.innerHTML = e.data;
+
+			collectors_entity.postMessage({
+				'module':'lond_constr',
+				'action':'add'
+			});
+		}	
+	}
+
+	function sayHello(callback_args){
+		console.log('hello');
 	}
 }
+
+
+
 
